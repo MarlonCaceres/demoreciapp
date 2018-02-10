febfe2dac4c53ca96040ad21f6ba5366e7f621cc
Marlon <m_ricky3021@hotmail.com>
2018-02-10 18:47:30 -0500

    Tour

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,11 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { Tour1PageModule } from '../pages/tour1/tour1.module';
+import { Tour2PageModule } from '../pages/tour2/tour2.module';
+import { Tour3PageModule } from '../pages/tour3/tour3.module';
+import { Tour4PageModule } from '../pages/tour4/tour4.module';
+import { InicioPageModule } from '../pages/inicio/inicio.module';
 
 @NgModule({
   declarations: [
@@ -14,7 +19,13 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    Tour1PageModule,
+    Tour2PageModule,
+    Tour3PageModule,
+    Tour4PageModule,
+    InicioPageModule
+
   ],
   bootstrap: [IonicApp],
   entryComponents: [
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+
+import { Tour1Page } from '../tour1/tour1'
 
 @Component({
   selector: 'page-home',
@@ -11,4 +13,9 @@ export class HomePage {
 
   }
 
+  Avanzar():void{
+  	this.navCtrl.push(Tour1Page);
+  }
+
+  
 }
